e93d9dbdf6bdecb40d18f4b4d2ffb42df983d61b
Nathan Chu <chu.nathan@gmail.com>
2020-05-26 14:26:17 -0400

    don't need this in overrides anymore

--- setEnv.sh
+++ setEnv.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-export APP_ID=`cat /usr/local/docker-config/APP_ID_HEX`
-export RESOURCE_ID=`cat  /usr/local/docker-config/RESOURCE_ID_HEX`
-export AUTH0_CLIENT_ID=`cat /usr/local/docker-config/httpd/psamaui_settings.json | grep client_id | cut -d ":" -f 2 | sed 's/\",*//g'`
 
-
-sed -i "s/__STACK_SPECIFIC_APPLICATION_ID__/$APP_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
-sed -i "s/__STACK_SPECIFIC_RESOURCE_UUID__/$RESOURCE_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
-sed -i "s/__AUTH0_CLIENT_ID__/$AUTH0_CLIENT_ID/g" ui/src/main/webapp/psamaui/settings/settings.json
 
 export PSAMA_SETTINGS_VOLUME=""
 export PICSURE_SETTINGS_VOLUME=""
