769a1431bc984ba7bb87bdeb847c4be0f456d585
Nathan Chu <chu.nathan@gmail.com>
2020-05-26 12:39:14 -0400

    fix some paths

--- setEnv.sh
+++ setEnv.sh
@@ -5,9 +5,9 @@ export RESOURCE_ID=`cat  /usr/local/docker-config/RESOURCE_ID_HEX`
 export AUTH0_CLIENT_ID=`cat /usr/local/docker-config/httpd/psamaui_settings.json | grep client_id | cut -d ":" -f 2 | sed 's/\",*//g'`
 
 
-sed -i "s/__STACK_SPECIFIC_APPLICATION_ID__/$APP_ID/g" ui/src/main/resources/picsureui/settings/settings.json
-sed -i "s/__STACK_SPECIFIC_RESOURCE_UUID__/$RESOURCE_ID/g" ui/src/main/resources/picsureui/settings/settings.json
-sed -i "s/__AUTH0_CLIENT_ID__/$AUTH0_CLIENT_ID/g" ui/src/main/resources/psamaui/settings/settings.json
+sed -i "s/__STACK_SPECIFIC_APPLICATION_ID__/$APP_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
+sed -i "s/__STACK_SPECIFIC_RESOURCE_UUID__/$RESOURCE_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
+sed -i "s/__AUTH0_CLIENT_ID__/$AUTH0_CLIENT_ID/g" ui/src/main/webapp/psamaui/settings/settings.json
 
 export PSAMA_SETTINGS_VOLUME=""
 export PICSURE_SETTINGS_VOLUME=""
