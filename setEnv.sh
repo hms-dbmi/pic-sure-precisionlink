8c0cea7dae4e2950f9f67fc2137f6f7cc39218c9
Nathan Chu <chu.nathan@gmail.com>
2020-06-08 15:45:58 -0400

    update settings to include auth0 domain entry

--- setEnv.sh
+++ setEnv.sh
@@ -11,11 +11,20 @@ fi
 
 export APP_ID=`cat $DOCKER_CONFIG_DIR/APP_ID_RAW`
 export RESOURCE_ID=`cat  $DOCKER_CONFIG_DIR/RESOURCE_ID_RAW`
-export AUTH0_CLIENT_ID=`cat $DOCKER_CONFIG_DIR/httpd/psamaui_settings.json | grep client_id | cut -d ":" -f 2 | sed 's/\",*//g'`
+
+if [ ! $AUTH0_CLIENT_ID ] ; then
+	export AUTH0_CLIENT_ID=`cat $DOCKER_CONFIG_DIR/httpd/psamaui_settings.json | grep client_id | cut -d ":" -f 2 | sed 's/\",*//g'`
+fi
+
+if [ ! $AUTH0_TENANT ] ; then
+	export AUTH0_TENANT=`cat $DOCKER_CONFIG_DIR/httpd/psamaui_settings.json | grep auth0domain | cut -d ":" -f 2 | sed 's/\",*//g'`
+fi
+
 
 sed -i "s/__STACK_SPECIFIC_APPLICATION_ID__/$APP_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
 sed -i "s/__STACK_SPECIFIC_RESOURCE_UUID__/$RESOURCE_ID/g" ui/src/main/webapp/picsureui/settings/settings.json
 sed -i "s/__AUTH0_CLIENT_ID__/$AUTH0_CLIENT_ID/g" ui/src/main/webapp/psamaui/settings/settings.json
+sed -i "s/__AUTH0_DOMAIN__/$AUTH0_TENANT/g" ui/src/main/webapp/psamaui/settings/settings.json
 
 if ! cmp -s "$DOCKER_CONFIG_DIR/httpd/psamaui_settings.json" "ui/src/main/webapp/psamaui/settings/settings.json"  ; then
 	cp ui/src/main/webapp/psamaui/settings/settings.json $DOCKER_CONFIG_DIR/httpd/psamaui_settings.json
