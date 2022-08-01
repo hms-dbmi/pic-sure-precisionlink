d72e82ae4caa7107cc2b7bcbbeb495cdba93cfb6
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2022-08-01 19:33:02 -0400

    Clean up an comment code so it's easier to understand.

--- ui/src/main/webapp/picsureui/overrides/filter/searchResults.js
+++ ui/src/main/webapp/picsureui/overrides/filter/searchResults.js
@@ -1,6 +1,6 @@
 define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResultTabs.hbs", "text!filter/searchResultSubCategories.hbs", "text!filter/searchResultSubCategoriesContainer.hbs", "picSure/settings"],
 		function($, searchResult, HBS, searchResultTabsTemplate, searchSubCatTemplate, searchSubCategoriesContainerTemplate, settings){
-	var searchResults = {
+	const searchResults = {
 			init : function(data, view, callback){
 				this.searchResultTabs = HBS.compile(searchResultTabsTemplate);
 				this.searchSubCategories = HBS.compile(searchSubCatTemplate);
@@ -8,26 +8,18 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 				this.addSearchResultRows(data, view, callback, view.model.get("searchTerm"));
 			}
 	};
-	searchResults.addSearchResultRows = function(data, filterView, queryCallback, searchTerm){
-		
-		//we want case INsensitive comparisons always
-		searchTerm = searchTerm.toLowerCase();
-		var getAliasName = function(key){
-			if(settings.categoryAliases && settings.categoryAliases[key]){
-                return settings.categoryAliases[key];
-            } else {
-                return key;
-            }
+	function getAliasName(key){
+		if(settings.categoryAliases && settings.categoryAliases[key]){
+			return settings.categoryAliases[key];
+		} else {
+			return key;
 		}
-		var keys = _.keys(data);
-		var aliases = [];
-		var aliasObjects = {}
+	}
+	function getAliasObjects(keys) {
+		let aliasObjects = {}
 		keys.forEach((key) => {
-			var alias = getAliasName(key)
-			if(aliases.indexOf(alias) == -1){
-				aliases.push(alias);
-			}
-			var aliasObj = aliasObjects[alias];
+			let alias = getAliasName(key)
+			let aliasObj = aliasObjects[alias];
 			if(aliasObj){
 				if(!aliasObj[key]){
 					aliasObj[key] = true;
@@ -42,49 +34,48 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 				aliasObjects[alias] = aliasObj;
 			}
 		});
-		
+		return aliasObjects;
+	}
+	searchResults.addSearchResultRows = function(data, filterView, queryCallback, searchTerm){
 		//track the category results
-		var parentCategorySearchResults = [];
-		
-		var compiledSubCategoryTemplate = this.searchSubCategories;
-		var compiledSubCategoryContainerTemplate = this.searchSubCategoriesContainer
-		// filterView.$el.hide();
+		const compiledSubCategoryTemplate = this.searchSubCategories;
+		const compiledSubCategoryContainerTemplate = this.searchSubCategoriesContainer;
+
+		//we want case INsensitive comparisons always
+		searchTerm = searchTerm.toLowerCase();
+
+		const categories = _.keys(data);
+		let aliases = categories.map(getAliasName);
+
 		$('.search-tabs', filterView.$el).append(this.searchResultTabs({
 			filterId: filterView.model.attributes.filterId, 
-			aliases: aliasObjects
+			aliases: getAliasObjects(categories)
 		}));
 		
- 		var categorySearchResultsByAlias = {};
-		keys.forEach((key) => {
-			var subCategories = [];
-			// var categorySearchResultViews = [];
-			var categorySearchResultViews = categorySearchResultsByAlias[getAliasName(key)];
-			
+		// -------- Render Categories
+ 		const categorySearchResultsByAlias = {};
+		categories.forEach(category => {
+			const subCategories = [];
+			const alias = getAliasName(category);
+
+			// -------- Render Category values
+			let categorySearchResultViews = categorySearchResultsByAlias[alias];
 			if(!categorySearchResultViews){
 				categorySearchResultViews = [];
-				categorySearchResultsByAlias[getAliasName(key)] = categorySearchResultViews;
+				categorySearchResultsByAlias[alias] = categorySearchResultViews;
 			}
-			_.each(data[key], function(value){
 
-				//trim off leading and trailing slashes.  !! Assume all data starts and ends with '\' !!
+			const parentCategorySearchResults = [];
+			_.each(data[category], function(value){
+				// trim off leading and trailing slashes.  !! Assume all data starts and ends with '\' !!
 				valuePath = value.data.substr(0, value.data.length-1).split('\\');
-					
-				//show only the highest level category or filter matching the search term
+				let categoryPath = undefined;
 				
-				//if this gets defined later, it will change filter clicks to 'anyRecordOf' clicks.
-				var categoryPath = undefined;
 				//loop over all of the categories; do not evaluate the final leaf node (we know it matches from the previous case)
-				//start from 2 here;  0 == empty; 1 = top level category (don't want to match)
-				
-				//
-				// Changing to include top level path for categories
-				//
-				before = valuePath
-				
 				for (i = 1; i < valuePath.length - 1; i++) {
 					if(valuePath[i].toLowerCase().includes(searchTerm.toLowerCase())){
 						categoryPath = valuePath.slice(0,i+1).join("\\");
-						//if we have already rendered this parent category, do not add another
+						
 						if(parentCategorySearchResults[categoryPath]){
 							return true;
 						}
@@ -99,7 +90,7 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 					}
 				}
 				
-				var matchedSelections = [];
+				let matchedSelections = [];
 				if(!categoryPath){
 					// For categorical or INFO columns, we want to render a search result for each value that matches the search term
 					if(value.columnDataType == "INFO"){
@@ -156,20 +147,17 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 				}
 				
 			});
-			// console.log('DEBUG (searchResults, 148): subCategories', subCategories, data[key])
-			data[key] = undefined;
+			
+			data[category] = undefined;
 			
 			//check to see if we have any valid results; we may have filtered them all out
 			if(categorySearchResultViews.length == 0){
 				//remove the 'category pill'
-				$('#'+getAliasName(key)+'-pill', filterView.$el).remove();
-				aliases = aliases.filter(item => item !== getAliasName(key))
+				$(`#${alias}-pill`, filterView.$el).remove();
+				aliases = aliases.filter(item => item !== alias)
 				return true; //then we don't need to worry about doing the subcategory work.
 			}
 
-			
-			
-			
 			_.each(categorySearchResultViews, function(newSearchResultRow){
         		//something is a little janky here, as we are seeing a funny 'description' string in the value field
 				// for gene info columns.  lets fix it.
@@ -179,17 +167,16 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 				newSearchResultRow.render();
 			});
 			
-			//save this tab object so we don't keep looking it up
-			var tabPane = $('#'+getAliasName(key)+'.tab-pane', filterView.$el)
-			console.log('DEBUG (searchResults, 164): subCategories count', subCategories, _.keys(subCategories).length)
-			
+			// -------- Render Sub Categories
+			let tabPane = $(`#${alias}.tab-pane`, filterView.$el);
 			if(_.keys(subCategories).length > 1){
 				//if no container has been added, add one for the sub categories
-				if($(".subcat-row", tabPane).length == 0)
+				if($(".subcat-row", tabPane).length == 0) {
 					$(".result-subcategories-div", tabPane).append(compiledSubCategoryContainerTemplate());
+				}
 
 				$(".sub-nav-pills", tabPane).append(compiledSubCategoryTemplate(_.keys(subCategories)));
-				// $(".result-subcategories-div", tabPane).append(compiledSubCategoryTemplate(_.keys(subCategories)));
+
 				//bootstap.js is used for the top-level category pills; here we are keeping a bit of the naming scheme
 				// need to roll our own logic so that the 'all results' sub-category tab works as expected
 				$(".sub-nav-pills li", tabPane).click(function(event){
@@ -197,14 +184,14 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 					$(event.target.parentElement).addClass("active")
 					$(event.target.parentElement).siblings().removeClass("active");
 					
-					// $('.tab-pane.active').hide();
+					$('.tab-pane.active').hide();
 					if(event.target.text == "All Results"){
 						_.each(categorySearchResultViews, function(result){
 							result.$el.show();
 						});
 					} else {
 						_.each(categorySearchResultViews, function(result){
-							var resultPath = result.model.attributes.data.substr(1, result.model.attributes.data.length-2).split('\\');
+							let resultPath = result.model.attributes.data.substr(1, result.model.attributes.data.length-2).split('\\');
 							if(resultPath.length > 1 && resultPath[1] == event.target.text){
 								result.$el.show();
 							} else {
@@ -212,22 +199,17 @@ define(["jquery", "filter/searchResult", "handlebars", "text!filter/searchResult
 							}
 						});
 					}
-					// $('.tab-pane.active').show();
+					$('.tab-pane.active').show();
 				});
 			}
-
-
 			$(".search-result-list", tabPane).append(_.pluck(categorySearchResultViews, "$el"));
-
-			
-
 		});
 
 		$("#"+_.first(aliases)).addClass("active");
 		$(".nav-pills li:first-child").addClass("active");
 		
 		//hide category selection if only a single category.
-		if(keys.length > 1) {
+		if(categories.length > 1) {
 			$(".filter-search > .nav-pills").show();
 		} else {
 			$(".filter-search > .nav-pills").hide();
